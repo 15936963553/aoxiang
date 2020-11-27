da937b3f521ab25c76e9a12e57c3fcf3e5df4b13
‘15936963553 <1753724838@qq.com>
2020-11-27 13:26:21 +0800

    'zz'

--- 找房无忧/01  上机练习需求及素材/练习1：找房无忧/Secon.js
+++ 找房无忧/01  上机练习需求及素材/练习1：找房无忧/Secon.js
@@ -1,32 +1,87 @@
 $('.nav1').on('touchstart',function(){
     $('.mengban').show()
-    $('.quyu').show()
+    var display=$('.quyu').css('display');
+   if( display=='none'){
+      $('.quyu').show()
+   }
+   if(display=='block'){
+    $('.quyu').hide()
+   }
+    
     $('.fangxing').hide()
     $('.more').hide()
     $('.price').hide()
 })
+
 $('.nav2').on('touchstart',function(){
     $('.mengban').show()
+    var display=$('.price').css('display');
+    if( display=='none'){
+       $('.price').show()
+    }
+    if(display=='block'){
+     $('.price').hide()
+    }
+
+
     $('.quyu').hide()
-    $('.price').show()
+   
+    $('#price').toggleClass('price')
     $('.fangxing').hide()
     $('.more').hide()
 })
 $('.nav3').on('touchstart',function(){
     $('.mengban').show()
+    var display=$('.fangxing').css('display');
+    if( display=='none'){
+       $('.fangxing').show()
+    }
+    if(display=='block'){
+     $('.fangxing').hide()
+    }
+
     $('.quyu').hide()
     $('.price').hide()
-    $('.fangxing').show()
+   
+    $('#fangxing').toggleClass('fangxing')
     $('.more').hide()
 })
 $('.nav4').on('touchstart',function(){
     $('.mengban').show()
+    var display=$('.more').css('display');
+    if( display=='none'){
+       $('.more').show()
+    }
+    if(display=='block'){
+     $('.more').hide()
+    }
+
     $('.quyu').hide()
     $('.price').hide()
     $('.fangxing').hide()
-    $('.more').show()
 })
 $('.mengban').on('touchstart',function(){
-    
     $('.mengban').hide()
+})
+
+
+
+
+$('nav span').on('touchstart',function(){
+    
+   
+
+
+    var display=$(this).find('.red_').css('display');
+     if(display=='none'){
+        $('.red_').hide()
+        $(this).find('.red_').show()
+     }else{$(this).find('.red_').hide()}
+
+     var display=$(this).find('.gray').css('display');
+     if(display=='block'){
+        $('.gray').show()
+        $(this).find('.gray').hide()
+     }else{$(this).find('.gray').show()}
+   
 })
